c1f58eef360da9506f6ac78d2550f5e4e9658f6d
lilianabs <lilianabsmath@gmail.com>
2022-11-01 11:58:16 -0600

    Rename files for consistency

--- pipelines/batch_inference_pipeline.py
+++ pipelines/batch_inference_pipeline.py
@@ -10,8 +10,14 @@ def batch_inference_pipeline(
 ):
     """Inference pipeline with skew and drift detection."""
     inference_data = load_inference_data()
-    model_deployment_service = prediction_service_loader()
-    predictor(model_deployment_service, inference_data)
-    training_data = load_training_data()
-    drift_detector(training_data, inference_data)
+    
+    
+    
+    
+    
+    
+    #model_deployment_service = prediction_service_loader()
+    #predictor(model_deployment_service, inference_data)
+    #training_data = load_training_data()
+    #drift_detector(training_data, inference_data)
 
