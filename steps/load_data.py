b9dbf71b5dbf47033a9b934e6ef4d0dc6adfdd2b
lilianabs <lilianabsmath@gmail.com>
2022-10-31 18:00:39 -0600

    Read from GCP bucket

--- steps/load_data.py
+++ steps/load_data.py
@@ -1,10 +1,11 @@
 import pandas as pd
+import os
 from zenml.steps import step, Output
 from .model_parameters import ModelParameters
 
 
 @step(enable_cache=False)
-def load_training_data() -> Output(data=pd.DataFrame):
+def load_training_data(parameters: ModelParameters) -> Output(data=pd.DataFrame):
 
     """Loads the train data
 
@@ -13,7 +14,10 @@ def load_training_data() -> Output(data=pd.DataFrame):
     """
     try:
         print("Loading training data")
-        data = pd.read_csv("data/train_test_split/train.csv")
+        absolute_path = os.path.dirname(__file__)
+        full_path = os.path.join(absolute_path, "..", parameters.SERVICE_ACCOUNT_FILE)
+        data = pd.read_csv(parameters.TRAIN_DATA_PATH, 
+                           storage_options={"token": full_path})
     except FileNotFoundError:
         print("CSV not found")
         data = pd.DataFrame()
@@ -31,7 +35,10 @@ def load_inference_data(parameters: ModelParameters) -> Output(data=pd.DataFrame
     """
     try:
         print("Loading inference data")
-        data = pd.read_csv("data/train_test_split/test.csv")
+        absolute_path = os.path.dirname(__file__)
+        full_path = os.path.join(absolute_path, "..", parameters.SERVICE_ACCOUNT_FILE)
+        data = pd.read_csv(parameters.TEST_DATA_PATH, 
+                           storage_options={"token": full_path})
     except FileNotFoundError:
         print("CSV not found")
         data = pd.DataFrame()
